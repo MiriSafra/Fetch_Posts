7f1d7b0ed16b3a05df7b84bcb0cd433d09b9a0cb
MiriSafra <m05333175827@gmail.com>
2025-06-17 22:39:32 +0300

    change

--- test_filter_posts.py
+++ test_filter_posts.py
@@ -6,7 +6,7 @@ posts = [
         "userId": 1,
         "id": 1,
         "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        "body": "quia!! et suscipit\nsuscipit recusandae..."
+        "body": "quia et suscipit\nsuscipit recusandae..."
     },
     {
         "userId": 2,
